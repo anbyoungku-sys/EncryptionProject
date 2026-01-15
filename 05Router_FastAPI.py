3f6edeb931c9bcdc2acd0bd7831e93f8781d5f89
anbyoungku-sys <anbyoungku@gmail.com>
2026-01-15 15:47:47 +0900

    라우터 기반 회원 관리 메인앱 구현

---
+++ 05Router_FastAPI.py
@@ -0,0 +1,42 @@
+from contextlib import asynccontextmanager
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from db import init_db
+from routers.member import router as router_member
+from routers.board import router as router_board
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router_member)
+app.include_router(router_board)
+
+@app.get("/", response_class=HTMLResponse)
+def index():
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="ko">
+        <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>FastAPI 회원/게시판앱</title>
+        </head>
+    <body>
+        <h1>FastAPI 회원/게시판앱</h1>
+        <ul>
+            <li><a href="/member/list">회원정보</a></li>
+            <li><a href="/board/list">게시판 목록</a></li>
+        </ul>
+    </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == "__main__":
+    import uvicorn
+    # uvicorn.run("파일명:객체명") 형식을 지켜야 하며 reload 모드 사용 시 중요함
+    uvicorn.run("05Router_FastAPI:app", host="0.0.0.0", port=8000, reload=True)
